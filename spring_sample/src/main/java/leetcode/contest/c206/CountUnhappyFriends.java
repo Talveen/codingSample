cd0b21cf0ffe0e94cb35a7f5aad14a34ac94890d
Talveen Rakhra <talveen.rakhra@hotmail.com>
2020-10-20 01:29:44 +0530

    project directory update

--- spring_sample/src/main/java/leetcode/contest/c206/CountUnhappyFriends.java
+++ spring_sample/src/main/java/leetcode/contest/c206/CountUnhappyFriends.java
@@ -1,6 +1,6 @@
 package leetcode.contest.c206;
 /*
-    Created By: Talveen Rakhra
+    Created By: Talveen Rakhra 
     Created On: 13-09-2020 
     Question URL: https://leetcode.com/contest/weekly-contest-206/problems/count-unhappy-friends/
     Time Taken: 
@@ -13,7 +13,7 @@ public class CountUnhappyFriends {
     public static void main(String[] args){
         CountUnhappyFriends countUnhappyFriends = new CountUnhappyFriends();
         int n = 4;
-        int[][] preferences = {{1,2,3},{3,2,0},{3,1,0},{1,2,0}};
+        int[][] preferences = {{1,2,3},{3,2,0},{3,1,0},{1,2,0},{7,2,1}};
         int[][] pairs={{0,1},{2,3}};
         System.out.println(countUnhappyFriends.unhappyFriends(n, preferences, pairs));
     }
